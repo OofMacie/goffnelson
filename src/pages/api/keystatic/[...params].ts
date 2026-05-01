5a58f831fd2124e3eed7c1088521360e93f13f7b
OofMacie <152102213+OofMacie@users.noreply.github.com>
2026-05-01 14:38:18 -0400

    chore: remove Keystatic configuration and related components

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,5 +0,0 @@
-export const prerender = false;
-import { makeHandler } from '@keystatic/astro/api';
-import keystaticConfig from '../../../keystatic.config';
-
-export const all = makeHandler({ config: keystaticConfig });
